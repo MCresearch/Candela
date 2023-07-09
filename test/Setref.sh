322042aaf9a3b3490e4eb72d262c4d646df30d94
Qianruipku <Terry_Liu@pku.edu.cn>
2023-07-09 17:43:21 +0800

    add tests for new abacus

--- test/Setref.sh
+++ test/Setref.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-allcases=`ls | grep ^[0-9]`
+if (($# == 0)); then
+    allcases=`ls | grep ^[0-9]`
+elif (($# > 0)); then
+    allcases=$@
+fi
 for case in $allcases
 do
     cd $case
     python3 ../tool/reset.py
     cd ../
-    
 done
